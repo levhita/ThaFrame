15c56d411a04d850ba4bf640aeedf0b0d04351d2
levhita <levhita@571d0474-d745-0410-b435-f446b85f08f0>
2011-01-06 21:41:04 +0000

    * Changed to Header an Footers to Layout (and yes this probably just broke every single template outhere. * Integrated UI javascript from current project Plus.
    git-svn-id: http://svn.thasystems.net/thaframe/trunk@108 571d0474-d745-0410-b435-f446b85f08f0

--- gateway/includes/functions.js
+++ gateway/includes/functions.js
@@ -1,7 +1,48 @@
+/** Functions fot the message Dialog **/
 function hideMessage() {
   var message = document.getElementById('message');
   message.style.visibility = "hidden";
 }
+
+/** Functions for the overlay **/
+function closeOverlay() {
+  document.getElementById('overlay').innerHTML ='';
+  document.getElementById('overlay').className ='hidden';
+  document.getElementById('overlay_shadow').className ='hidden';
+}
+
+function showOverlay(content) {
+  document.getElementById('overlay').className ='visible';
+  document.getElementById('overlay').innerHTML = content;
+  document.getElementById('overlay_shadow').className ='visible';
+}
+
+/**
+ * @todo fix languaje issues
+ * @todo fix url issues
+ * @param to_root
+ * @param image
+ * @return void
+ */
+function overlayImage(to_root, image) {
+	content ="<img src='"+image+"' /> <ul class='action'><li><a href='javascript:closeOverlay();' title='Volver'><img src='"+to_root+"/images/toolbars/undo.png' alt='Back' /></a></li></ul>";
+	showOverlay(content); 
+}
+
+/** Paging functions for Listings **/
+function change_page(element, url){
+  var page_number = valSelect(element);
+  url = url.replace('replace_with_page_number', page_number);
+  window.location = url;
+}
+
+function change_page_size(element, url){
+  var page_size = valSelect(element);
+  url = url.replace('replace_with_page_size', page_size);
+  window.location = url;
+}
+
+/** Generic buttons to get the value of various HTML Input Elements **/
 function valRadioButton(radio_button)
 {
   for (i=radio_button.length-1; i > -1; i--) {
@@ -17,14 +58,15 @@ function valSelect(select)
   return select.options[select.selectedIndex].value;
 }
 
-function change_page(element, url){
-  var page_number = valSelect(element);
-  url = url.replace('replace_with_page_number', page_number);
-  window.location = url;
-}
-
-function change_page_size(element, url){
-  var page_size = valSelect(element);
-  url = url.replace('replace_with_page_size', page_size);
-  window.location = url;
+/** Generic Form functions **/
+function focusOnFirst() {
+  if (document.forms.length > 0) {
+	for (var i=0; i < document.forms[0].elements.length; i++) {
+      var oField = document.forms[0].elements[i];
+      if (oField.type != "hidden") {
+        oField.focus();
+        return;
+      }
+	}
+  }
 }
--- patterns/Page.inc.php
+++ patterns/Page.inc.php
@@ -18,140 +18,95 @@ class Page
    * Holds the variables to be passed to the template as $Data object
    * @var array
    */
-  protected $variables = array();
+  protected $_variables = array();
   
   /**
    * Holds the javascripts that should be added at the head section
    * @var array
    */
-  protected $javascripts = array();
+  protected $_javascripts = array();
   
   /**
    * Holds the relative path to the template
    * @var string
    */
-  protected $template = '';
+  protected $_template = '';
+  
+  /**
+   * Holds the relative path to the layout
+   * @var string
+   */
+  protected $_layout = '';
   
   /**
    * Script to run on page load
    *
    * @var string
    */
-  public $on_load   = '';
+  public $_on_load   = '';
   
   /**
    * Holds the header to be sent on_display
    *
    * @var array
    */
-  public $headers = array();
+  public $_headers = array();
 
   /**
    * Variables that belongs only to this pattern, used to customize the text and
    * appareance of the page
    * @var array
    */
-  protected $pattern_variables = array();
+  protected $_pattern_variables = array();
   /**
    * Holds the main menu template
    * @var string
    */
-  public $main_menu_template = '';
+  protected $_main_menu_template = '';
   /**
    * Holds the secondary menu template
    * @var string
    */
-  public $secondary_menu_template = '';
+  protected $_secondary_menu_template = '';
   
-  public function __construct($page_name='', $template='')
+  
+  public function __construct($page_name='', $template='', $layout='')
   {
-    if ( empty($template) ) {
+    if( empty($template) ){
       $template = $this->getScriptName();
     }
 
+    $this->setPageName($page_name);
     $this->setTemplate($template);
-  	$this->assign('page_name', $page_name);
+    $this->setLayout($layout);
   }
   
-  /**
-   * Shows the given template
-   *
-   * Converts the $variables array into $Data object and sets any message that may
-   * be in the $_SESSION and finally calls the given template
-   * @return void
-   */
-  public function display()
-  {
-    if ( !file_exists($this->template) ) {
-      throw new InvalidArgumentException("'$this->template' doesn't exists");
-    }
-    if( isset($_SESSION['__message_text']) ) {
-      $message = array(
-        'level' => $_SESSION['__message_level'] ,
-        'text' => $_SESSION['__message_text']
-      );
-      
-      $this->assign('__message', $message);
-      unset($_SESSION['__message_text']);
-      unset($_SESSION['__message_level']);
-      unset($message);
-    }
-    $script_name = $this->getScriptName();
-    if ( empty($this->main_menu_template) ) {
-      if ( file_exists("templates/" . $this->getScriptName() . "_menu.tpl.php") ) {
-        $this->setMainMenu($this->getScriptName()."_menu");
-      } else if (file_exists('templates/default_main_menu.tpl.php') ) {
-        $this->setMainMenu('default_main_menu');
-      } else if (file_exists(TO_ROOT. "/subtemplates/default_main_menu.tpl.php") ) {
-        $this->setMainMenu(TO_ROOT. '/subtemplates/default_main_menu.tpl.php', TRUE);
-      } else {
-        $this->setMainMenu(THAFRAME. '/subtemplates/default_main_menu.tpl.php', TRUE);
-      }
-    }
-    
-    if ( empty($this->secondary_menu_template) ) {
-      if ( file_exists("templates/" . $this->getScriptName() . "_menu.tpl.php") ) {
-        $this->setSecondaryMenu($this->getScriptName()."_menu");
-      } else if (file_exists('templates/default_secondary_menu.tpl.php') ) {
-        $this->setSecondaryMenu('default_secondary_menu');
-      } else if (file_exists(TO_ROOT. "/subtemplates/default_secondary_menu.tpl.php") ) {
-        $this->setSecondaryMenu(TO_ROOT. '/subtemplates/default_secondary_menu.tpl.php', TRUE);
-      } else {
-        $this->setSecondaryMenu(THAFRAME. '/subtemplates/default_secondary_menu.tpl.php', TRUE);
-      }
-    }
-    
-    $this->assign('PatternVariables', (object)$this->pattern_variables);
-    $this->assign('javascripts', $this->javascripts);
-    $this->assign('main_menu_template', $this->main_menu_template);
-    $this->assign('secondary_menu_template', $this->secondary_menu_template);
-    $this->assign('__on_load', $this->on_load);
-    
-    /** Convert all variables into an object **/
-    $Data = (object)$this->variables;
-    
-    /** This object actually helps to do a variety of thisn inside templates
-     * @var Helper
-     */
-    $Helper = new Helper($Data);
-    
-    /** Sends the headers **/
-    foreach($this->headers AS $header => $value)
-    {
-      header("$header: $value");
-    }
-    
-    include $this->template;
-  }
+
   
   /**
-   * Adds a javascript that will be added in the head section
-   * @param string $javascript the javascript code
-   * @return void
+   * Jumps to the given url, sending a message.
+   * @param string $url 
+   * @param string $message
+   * @param string $level might be: info, warning, error, success 
+   * @return false
    */
-  public function addJavascript($javascript)
+  public function goToPage($url, $message = '', $level='info') {
+    if ( $message ) {
+      $_SESSION['__message_text'] = $message;
+      $_SESSION['__message_level'] = $level;
+    }
+    header("location: $url");
+    die();
+  }
+  
+  public function getScriptName(){
+    return basename($_SERVER['SCRIPT_FILENAME'], '.php');
+  }
+
+  
+  public function assign($variable, $value)
   {
-    $this->javascripts[] = $javascript;
+    $this->_variables[$variable] = $value;
   }
   
   /**
@@ -165,35 +120,49 @@ class Page
    */
   public function setTemplate($template, $fullpath = false)
   {
-    if ( !$fullpath) {
-      $this->template = "templates/$template.tpl.php";
+    if (!$fullpath) {
+      $this->_template = "templates/$template.tpl.php";
     } else {
-      $this->template = $template;
+      $this->_template = $template;
     }
   }
   
-  public function assign($variable, $value)
+  /**
+   * Sets the layout file to be used.
+   *
+   * Take for granted that the file is under the relative path "templates/" and
+   * has a "tpl.php" extension, unless you set $fullpath to true
+   * @param string  $layout The name of the layout to be used
+   * @param bool    $fullpath overrides the naming convention and allows you to set any file
+   * @return void
+   */
+  public function setLayout($layout, $fullpath = false)
   {
-    $this->variables[$variable] = $value;
-  }
-  
-  public function goToPage($url, $message = '', $level='info') {
-    if ( $message ) {
-      $_SESSION['__message_text'] = $message;
-      $_SESSION['__message_level'] = $level;
+    if ( !empty($layout) && !$fullpath){
+      $this->_layout = "templates/$layout.tpl.php";
+    } else {
+      $this->_layout = $layout;
     }
-    header("location: $url");
-    die();
   }
   
-  public function getScriptName(){
-    return basename($_SERVER['SCRIPT_FILENAME'], '.php');
+  public function setMainMenu($template, $fullpath = false)
+  {
+    if ( !empty($template) && !$fullpath) {
+      $this->_main_menu_template = "templates/$template.tpl.php";
+    } else {
+      $this->_main_menu_template = $template;
+    }
+  }
+    
+  public function setSecondaryMenu($template, $fullpath = false)
+  {
+    if ( !empty($template) && !$fullpath) {
+      $this->_secondary_menu_template = "templates/$template.tpl.php";
+    } else {
+      $this->_secondary_menu_template = $template;
+    }
   }
   
-  public function setOnLoad($code=''){
-    $this->on_load=$code;
-  }
-
   /**
    * Sets a pattern specific variable, variables set by this function aren't
    * mandatory, and are only to provide customization to the default template
@@ -203,7 +172,25 @@ class Page
    * @return void
    */
   public function setPatternVariable($variable, $value)  {
-    $this->pattern_variables[$variable] = $value;
+    $this->_pattern_variables[$variable] = $value;
+  }
+  
+  /**
+   * Sets the page name depending of the layout it might be translated
+   * @param string $page_name
+   * @return void
+   */
+  public function setPageName($page_name) {
+    $this->assign('__page_name', $page_name);
+  }
+  
+  /** Sets the javascript code to be run when the pase finishes loading
+   * 
+   * @param string $code javascript code
+   * @return void
+   */   
+  public function setOnLoad($code=''){
+    $this->_on_load=$code;
   }
   
   /**
@@ -213,24 +200,109 @@ class Page
    * @param string $value
    */
   public function addHeader($header, $value){
-    $this->headers[$header]=$value;
+    $this->_headers[$header]=$value;
   }
   
-  public function setMainMenu($template, $fullpath = false)
+  /**
+   * Adds a javascript that will be added in the head section
+   * @param string $javascript the javascript code
+   * @return void
+   */
+  public function addJavascript($javascript)
   {
-    if ( !$fullpath) {
-      $this->main_menu_template = "templates/$template.tpl.php";
-    } else {
-      $this->main_menu_template = $template;
-    }
+    $this->_javascripts[] = $javascript;
   }
-    
-  public function setSecondaryMenu($template, $fullpath = false)
+
+  
+  /**
+   * Shows the given template
+   *
+   * Converts the $variables array into $Data object and sets any message that may
+   * be in the $_SESSION and finally calls the given template
+   * @return void
+   */
+  public function display()
   {
-    if ( !$fullpath) {
-      $this->secondary_menu_template = "templates/$template.tpl.php";
-    } else {
-      $this->secondary_menu_template = $template;
+
+    if( isset($_SESSION['__message_text']) ) {
+      $message = array(
+        'level' => $_SESSION['__message_level'] ,
+        'text' => $_SESSION['__message_text']
+      );
+      
+      $this->assign('__message', $message);
+      unset($_SESSION['__message_text']);
+      unset($_SESSION['__message_level']);
+      unset($message);
     }
+    
+    /** Main Menu Cascade Loader **/
+    $script_name = $this->getScriptName();
+    if ( empty($this->_main_menu_template) ) {
+      if ( file_exists("templates/" . $this->getScriptName() . "_menu.tpl.php") ) {
+        $this->setMainMenu($this->getScriptName()."_menu");
+      } else if (file_exists('templates/default_main_menu.tpl.php') ) {
+        $this->setMainMenu('default_main_menu');
+      } else if (file_exists(TO_ROOT. "/subtemplates/default_main_menu.tpl.php") ) {
+        $this->setMainMenu(TO_ROOT. '/subtemplates/default_main_menu.tpl.php', TRUE);
+      } else {
+        $this->setMainMenu(THAFRAME. '/subtemplates/default_main_menu.tpl.php', TRUE);
+      }
+    }
+    
+    /** Secondary Menu Cascade Loader**/
+    if ( empty($this->_secondary_menu_template) ) {
+      if ( file_exists("templates/" . $this->getScriptName() . "_menu.tpl.php") ) {
+        $this->setSecondaryMenu($this->getScriptName()."_menu");
+      } else if (file_exists('templates/default_secondary_menu.tpl.php') ) {
+        $this->setSecondaryMenu('default_secondary_menu');
+      } else if (file_exists(TO_ROOT. "/subtemplates/default_secondary_menu.tpl.php") ) {
+        $this->setSecondaryMenu(TO_ROOT. '/subtemplates/default_secondary_menu.tpl.php', TRUE);
+      } else {
+        $this->setSecondaryMenu(THAFRAME. '/subtemplates/default_secondary_menu.tpl.php', TRUE);
+      }
+    }
+    
+    /** Layout Cascade Loader **/
+    if ( empty($this->_layout) ) {
+      if ( file_exists("templates/" . $this->getScriptName() . "_layout.tpl.php") ) {
+        $this->setLayout($this->getScriptName()."_menu");
+      } else if (file_exists('templates/default_layout.tpl.php') ) {
+        $this->setLayout('default_layout');
+      } else if (file_exists(TO_ROOT. "/subtemplates/default_layout.tpl.php") ) {
+        $this->setLayout(TO_ROOT. '/subtemplates/default_layout.tpl.php', TRUE);
+      } else {
+        $this->setLayout(THAFRAME. '/subtemplates/default_layout.tpl.php', TRUE);
+      }
+    }
+    
+    $this->assign('PatternVariables', (object)$this->_pattern_variables);
+    $this->assign('__javascripts', $this->_javascripts);
+    $this->assign('__main_menu_template', $this->_main_menu_template);
+    $this->assign('__secondary_menu_template', $this->_secondary_menu_template);
+    $this->assign('__on_load', $this->_on_load);
+    
+    /** Convert all variables into an object **/
+    $Data = (object)$this->_variables;
+    
+    /** This object actually helps to do a variety of things inside templates
+     * @var Helper
+     */
+    $Helper = new Helper($Data);
+    
+    /** Sends the headers **/
+    foreach($this->_headers AS $header => $value)
+    {
+      header("$header: $value");
+    }
+    
+    $_content_ = '';
+    ob_start();
+      include $this->_template;
+      $_content_ = ob_get_contents();
+    ob_end_clean();
+    
+    /** Include it in the Layout **/
+    include $this->_layout;
   }
 }
