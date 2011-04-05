97b7e2666ec3e98c413b0a7813cca87aae4c7518
Argel Arias <levhita@gmail.com>
2011-04-05 18:29:24 -0500

    * Fixed Permissions * Fixed Naming Conventions * Fixed default_layout with new conventions

--- models/templates/task_full.tpl.php
+++
@@ -1,4 +0,0 @@
-<div id="task_<?php echo $Data->task_id ?>"
-  class="task <?php echo ($Data->done=='1')?'done':'pending'; ?>" >
-  <?php include TO_ROOT . "/models/templates/task_innerhtml.tpl.php"; ?>
-</div>
